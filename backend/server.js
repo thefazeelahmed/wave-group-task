48783ca904de3d7303a01e2f5a1681b6b35b1f15
Fazeel Ahmed <58031420+thefazeelahmed@users.noreply.github.com>
2025-05-15 18:43:26 +0500

    chores : fixed files with prettier setup

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const initializeDatabase = require('./src/db/init');
 require('dotenv').config();
 const userRoutes = require('./src/routes/user');
+const uploadRoutes = require('./src/middleware/upload');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
